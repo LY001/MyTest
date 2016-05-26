f8c7700a5caaf4ecb0872d73c0d65eb43ebc357b
lanouhn <123437792@qq.com>
2016-05-26 17:10:43 +0800

    新建了一个工程

---
+++ 666666/666666Tests/_66666Tests.m
@@ -0,0 +1,39 @@
+//
+//  _66666Tests.m
+//  666666Tests
+//
+//  Created by lanouhn on 16/5/26.
+//  Copyright © 2016年 lanouhn. All rights reserved.
+//
+
+#import <XCTest/XCTest.h>
+
+@interface _66666Tests : XCTestCase
+
+@end
+
+@implementation _66666Tests
+
+- (void)setUp {
+    [super setUp];
+    // Put setup code here. This method is called before the invocation of each test method in the class.
+}
+
+- (void)tearDown {
+    // Put teardown code here. This method is called after the invocation of each test method in the class.
+    [super tearDown];
+}
+
+- (void)testExample {
+    // This is an example of a functional test case.
+    // Use XCTAssert and related functions to verify your tests produce the correct results.
+}
+
+- (void)testPerformanceExample {
+    // This is an example of a performance test case.
+    [self measureBlock:^{
+        // Put the code you want to measure the time of here.
+    }];
+}
+
+@end
